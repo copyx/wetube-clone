720b4dbed0f214197680a3b0de5a1379dd7e6ceb
Jingi Kim <copyx00@gmail.com>
2020-04-28 23:41:08 +0900

    Babel과 미들웨어들 추가

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 const app = express();
 const port = 3000;
 
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("common"));
+
 app.get("/", (req, res) => res.send("Hello World!"));
+app.use(betweenHome);
+app.get("/test", (req, res) => res.send("This is test!"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
