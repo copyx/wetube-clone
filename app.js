79e5a4944e93c37516a3cf64e68c09f8c8091034
Jingi Kim <copyx00@gmail.com>
2020-05-10 01:07:52 +0900

    템플릿에 스크립트 및 locals 추가

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
 app.set("views", "./views");
 app.set("view engine", "pug");
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("common"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
