d58a18359a61e9636a3077bcbdb39de9e1060403
Jingi Kim <copyx00@gmail.com>
2020-05-12 22:18:38 +0900

    Home 화면에 Videos HTML 추가

---
+++ db.js
@@ -0,0 +1,41 @@
+export const videos = [
+  {
+    id: 25344,
+    title: "Good Video",
+    description: "This is good.",
+    views: 33,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 34782,
+      name: "Jingi",
+      email: "copyx00@gmail.com",
+    },
+  },
+  {
+    id: 92834,
+    title: "Better Video",
+    description: "This is better.",
+    views: 33,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 15324,
+      name: "JK",
+      email: "copyx00@gmail.com",
+    },
+  },
+  {
+    id: 2134,
+    title: "Best Video",
+    description: "This is Best.",
+    views: 33,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 75464,
+      name: "Jake",
+      email: "copyx00@gmail.com",
+    },
+  },
+];
