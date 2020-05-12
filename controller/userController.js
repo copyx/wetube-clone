da51f7ecc50eedf2db672bdf4df4bcb651b5ee32
Jingi Kim <copyx00@gmail.com>
2020-05-12 22:51:42 +0900

    UserController의 Join 로직 추가

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,22 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, passwordToVerify },
+  } = req;
+
+  if (password !== passwordToVerify) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    // TODO: Register user
+    // TODO: Log user in
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
