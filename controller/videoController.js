aacacffebc9653562ab4730de83b732e938e8e86
Jingi Kim <copyx00@gmail.com>
2020-05-17 14:55:17 +0900

    Logout & Upload 페이지 이동 연결
    - 필수정보 입력 속성 추가

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,4 +1,6 @@
 import { videos } from "../db";
+import routes from "../routes";
+
 export const home = (req, res) =>
   res.render("home", { pageTitle: "Home", videos });
 export const search = (req, res) => {
@@ -7,8 +9,17 @@ export const search = (req, res) => {
   } = req;
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description },
+  } = req;
+
+  //TODO: Upload and save video
+
+  res.redirect(`${routes.videos}${routes.videoDetail(25344)}`);
+};
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Details" });
 export const editVideo = (req, res) =>
