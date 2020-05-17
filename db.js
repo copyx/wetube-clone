6bef2613accb89c827ed522d02c5185d43c19fa8
Jingi Kim <copyx00@gmail.com>
2020-05-17 15:16:54 +0900

    Mongoose를 이용한 DB 연결 추가

--- db.js
+++ db.js
@@ -1,41 +1,15 @@
-export const videos = [
-  {
-    id: 25344,
-    title: "Good Video",
-    description: "This is good.",
-    views: 33,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 34782,
-      name: "Jingi",
-      email: "copyx00@gmail.com",
-    },
-  },
-  {
-    id: 92834,
-    title: "Better Video",
-    description: "This is better.",
-    views: 33,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 15324,
-      name: "JK",
-      email: "copyx00@gmail.com",
-    },
-  },
-  {
-    id: 2134,
-    title: "Best Video",
-    description: "This is Best.",
-    views: 33,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 75464,
-      name: "Jake",
-      email: "copyx00@gmail.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/wetube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("👍 Connected to DB");
+});
+db.on("error", (error) => {
+  console.error(`🚨 Error on DB Connection: ${error}`);
+});
