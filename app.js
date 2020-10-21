726a427e9ef0e7e85fbee5fd6ba119c064524551
Jingi Kim <copyx00@gmail.com>
2020-10-21 17:40:26 +0900

    Add Pug as view engine and templates

--- app.js
+++ app.js
@@ -8,14 +8,29 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://*.fontawesome.com/"],
+        styleSrc: ["'self'", "https://*.fontawesome.com/"],
+        fontSrc: ["'self'", "https://*.fontawesome.com/"],
+      },
+    },
+  })
+);
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
