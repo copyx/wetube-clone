ff3fef2d84b57710f099635ad304a0204b2032b0
Jingi Kim <copyx00@gmail.com>
2020-10-23 19:02:51 +0900

    Implement uploading video logic
    Etc.
    - Change ecmaVersion in .eslintrc

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,26 +1,37 @@
-import { videos } from "../db";
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.error(`❌ Error: ${error}`);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy, videos });
+  res.render("search", { pageTitle: "Search", searchingBy, videos: [] });
 };
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description },
+    body: { title, description },
+    file: { path },
   } = req;
 
-  // TODO: Upload and save video
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
 
-  res.redirect(routes.videos + routes.videoDetail(1234894));
+  res.redirect(routes.videos + routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
