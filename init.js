29749c4c403c1c98acf14d540ce573c05cb60217
Jingi Kim <copyx00@gmail.com>
2020-10-23 15:39:18 +0900

    Setup mongoose and dotenv

--- init.js
+++ init.js
@@ -1,7 +1,10 @@
-import app from './app'
+import "./db";
+import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`✅ Listening on: http://localhost:${port}`);
 });
